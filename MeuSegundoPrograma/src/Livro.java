16dc6cd73d05741c50171c89db73305986ae3d9e
luizhakan <luizhakan2@protonmail.com>
2023-06-06 21:13:54 -0300

    feat: finalizando o terceiro cap, sobre oop

--- MeuSegundoPrograma/src/Livro.java
+++ MeuSegundoPrograma/src/Livro.java
@@ -3,7 +3,19 @@ public class Livro {
         String nome;
         String email;
         String cpf;
+
+        void mostrarDetalhes(){
+            System.out.printf("\nO livro foi escrito por %s, cujo CPF é: %s e email é: %s %n--\n", nome, cpf, email);
+        };
     }
+
+//    public Livro(){
+//        System.out.printf("Novo livro criado\n");
+//    } 
+
+    boolean temAutor(){
+        return this.autor != null;
+    };
 
     String nome;
     String descricao;
@@ -12,15 +24,27 @@ public class Livro {
     Autor autor;
 
     void mostrarDetalhes(){
-        System.out.printf("O livro %s identificado pelo ISBN: %s, custa R$%.2f. O livro foi escrito por %s, cujo CPF é: %s e email é: %s %n--\n",  nome, isbn, valor, autor.nome, autor.cpf, autor.email);
+        System.out.printf("O livro %s identificado pelo ISBN: %s, custa R$%.2f.",  nome, isbn, valor);
+        if (this.temAutor()){
+          autor.mostrarDetalhes();
+        } else {
+            System.out.printf("\nNão tem detalhe do autor%n--\n");
+        };
+    };
+
+    public void aplicaDescontoDe(double porcentagem){
+      valor -= this.valor * porcentagem;
     };
 
     public static void main(String[] args){
+        /* Livro livro1 = new Livro();
+        Livro livro2 = new Livro();
+        Livro livro3 = new Livro(); */
+
         Autor autor = new Autor();
         autor.nome = "Luiz Hakan";
         autor.email = "luizhakan2@protonmail.com";
         autor.cpf = "132.456.789-10";
-
 
         Livro livro = new Livro();
         livro.nome = "Java 8 Prático";
@@ -38,7 +62,7 @@ public class Livro {
         outroLivro.descricao = "Crie seus primeiros programas";
         outroLivro.valor = 59.90;
         outroLivro.isbn = "978-85-66250-22-0";
-        outroLivro.autor = autor;
+        //  outroLivro.autor = autor;
 
         livro.mostrarDetalhes();
         outroLivro.mostrarDetalhes();
